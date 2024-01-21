e0ced532ae1c34737826671b998ad27dec6b5b8f
Enoch Taylor <tay121222@gmail.com>
2024-01-21 20:55:37 +0000

    method that calculates the fewest number of operations needed to result in exactly n H characters in the file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Contains method minOperations"""
+import math
+
+
+def minOperations(n):
+    """method that calculates the fewest number of
+    operations needed to result in exactly"""
+    if n == 1:
+        return 0
+    result = 0
+    i = 2
+    while i * i <= n:
+        while n % i == 0:
+            n //= i
+            result += i
+        i += 1
+    if n > 1:
+        result += n
+
+    return result
