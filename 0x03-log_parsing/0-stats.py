d2ce6747722daa1c63b9952eb42e12f3fc01e9e4
Enoch Taylor <tay121222@gmail.com>
2024-01-28 23:08:12 +0000

    script that reads stdin line by line and computes metrics

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""script that reads stdin line by line and computes metrics"""
+import sys
+
+
+def print_statistics(file_size, status_counts):
+    """Prints file size and status code"""
+    print("File size: {}".format(file_size))
+    for code in sorted(status_counts.keys()):
+        if status_counts[code] > 0:
+            print("{}: {}".format(code, status_counts[code]))
+
+
+if __name__ == '__main__':
+    total_file_size = 0
+    status_code_counts = {
+            code: 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]
+            }
+    line_count = 0
+
+    try:
+        for line in sys.stdin:
+            line_count += 1
+            parts = line.strip().split()
+
+            try:
+                status_code = int(parts[-2])
+                file_size = int(parts[-1])
+
+                if status_code in status_code_counts:
+                    status_code_counts[status_code] += 1
+
+                total_file_size += file_size
+
+                if line_count % 10 == 0:
+                    print_statistics(total_file_size, status_code_counts)
+
+            except (ValueError, IndexError):
+                continue
+
+        print_statistics(total_file_size, status_code_counts)
+
+    except KeyboardInterrupt:
+        print_statistics(total_file_size, status_code_counts)
