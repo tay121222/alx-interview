c995e5e36940afe5f8d0b3bb947a1cadb2f814df
Enoch Taylor <tay121222@gmail.com>
2024-03-14 09:46:14 +0000

    Function to determine determine who the winner of each game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Contains isWinner function to determine
+who the winner of each game"""
+
+
+def isWinner(x, nums):
+    """determine who the winner of each game"""
+    def can_win(round_nums):
+        """Check if Maria can win the current round"""
+        total_primes = sum(1 for num in range(
+            1, max(round_nums) + 1) if is_prime(num)
+            )
+        return total_primes % 2 == 1
+
+    def is_prime(num):
+        """Check if a number is prime"""
+        if num < 2:
+            return False
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if can_win(range(1, n + 1)):
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
