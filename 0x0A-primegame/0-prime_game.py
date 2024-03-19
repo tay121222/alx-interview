48c0065fdbb4285f8c9612ea3142af12c31e5f3a
Enoch Taylor <tay121222@gmail.com>
2024-03-19 05:40:42 +0000

    Fixed code to determine actual winner of each round

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,36 +4,36 @@ who the winner of each game"""
 
 
 def isWinner(x, nums):
-    """determine who the winner of each game"""
-    def can_win(round_nums):
-        """Check if Maria can win the current round"""
-        remaining = list(range(2, max(round_nums) + 1))
-        for num in range(2, max(round_nums) + 1):
-            if is_prime(num):
-                remaining = [n for n in remaining if n % num != 0]
-        return len(remaining) % 2 == 1
+    def sieve(n):
+        """Genetare list of prime numbers"""
+        primes = []
+        sieve = [True] * (n + 1)
+        sieve[0] = sieve[1] = False
+        for p in range(2, n + 1):
+            if sieve[p]:
+                primes.append(p)
+                for i in range(p * p, n + 1, p):
+                    sieve[i] = False
+        return primes
 
-    def is_prime(num):
-        """Check if a number is prime"""
-        if num < 2:
-            return False
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
+    def canWin(n):
+        """check winner of each round"""
+        primes = sieve(n)
+        remaining = set(range(1, n + 1))
+        for p in primes:
+            if p in remaining:
+                remaining.difference_update(range(p, n + 1, p))
+            else:
+                for multiple in range(p, n + 1, p):
+                    if multiple in remaining:
+                        remaining.remove(multiple)
+        return len(remaining) > 0
 
-    maria_wins = 0
-    ben_wins = 0
+    maria_wins = sum(canWin(n) for n in nums)
 
-    for n in nums:
-        if can_win(range(1, n + 1)):
-            maria_wins += 1
-        else:
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
+    if maria_wins > x - maria_wins:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif maria_wins < x - maria_wins:
         return "Ben"
     else:
         return None
