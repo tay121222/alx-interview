788959c7b13c90491959aa45d7bbcfae0fd0b800
Enoch Taylor <tay121222@gmail.com>
2024-03-19 11:14:46 +0000

    remodified code to determine the best winner

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,37 +4,49 @@ who the winner of each game"""
 
 
 def isWinner(x, nums):
-    """Find winner of each game"""
-    def sieve(n):
-        """Genetare list of prime numbers"""
-        primes = []
-        sieve = [True] * (n + 1)
-        sieve[0] = sieve[1] = False
-        for p in range(2, n + 1):
-            if sieve[p]:
-                primes.append(p)
-                for i in range(p * p, n + 1, p):
-                    sieve[i] = False
-        return primes
+    """Determine the winner of the game"""
+    maria_wins = 0
+    ben_wins = 0
 
-    def canWin(n):
-        """check winner of each round"""
-        primes = sieve(n)
-        remaining = set(range(1, n + 1))
-        for p in primes:
-            if p in remaining:
-                remaining.difference_update(range(p, n + 1, p))
-            else:
-                for multiple in range(p, n + 1, p):
-                    if multiple in remaining:
-                        remaining.remove(multiple)
-        return len(remaining) > 0
+    for num in nums:
+        rounds_set = list(range(1, num + 1))
+        primes_set = primes_in_range(1, num)
 
-    maria_wins = sum(canWin(n) for n in nums)
+        if not primes_set:
+            ben_wins += 1
+            continue
 
-    if maria_wins > x - maria_wins:
+        is_maria_turn = True
+
+        while primes_set:
+            smallest_prime = primes_set.pop(0)
+            rounds_set.remove(smallest_prime)
+            rounds_set = [x for x in rounds_set if x % smallest_prime != 0]
+            is_maria_turn = not is_maria_turn
+
+        if is_maria_turn:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins > ben_wins:
         return "Maria"
-    elif maria_wins < x - maria_wins:
+    elif maria_wins < ben_wins:
         return "Ben"
     else:
         return None
+
+
+def is_prime(n):
+    """Checks if number is prime number"""
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def primes_in_range(start, end):
+    """Generate Prime number"""
+    return [n for n in range(start, end + 1) if is_prime(n)]
